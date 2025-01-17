d3df81ad48541684fdec9d471e638b08d710b46b
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-01-17 14:04:21 +0200

    update

--- database/migrations/2024_12_27_134739_create_products_table.php
+++ database/migrations/2024_12_27_134739_create_products_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->decimal('purchesPrice');
             $table->decimal('profit');
             $table->String('image')->nullable();
-            $table->timestamp('creationDate')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
